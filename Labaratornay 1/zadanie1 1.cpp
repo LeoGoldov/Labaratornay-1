2fec54b066d0e3f8424a6a38cf5330c87e3d0fcd
LeoGoldov <levfeb@gmail.com>
2025-06-02 12:38:25 +0300

    Update zadanie1 1.cpp

--- Labaratornay 1/zadanie1 1.cpp
+++ Labaratornay 1/zadanie1 1.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main()
 {
-    setlocale(LC_ALL, "Russian");  // Исправлено "RU" на "Russian"
+    setlocale(LC_ALL, "Russian"); 
     system("chcp 1251");
 
     ifstream in;
